15bff7ebe7900befcf6355c6766b9a6c9699c5a1
Thauan <thauansilva243@gmail.com>
2025-03-18 09:46:22 -0400

    feat: add router in header and change placeholder

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,19 @@
+"use client";
 import { Search } from "lucide-react";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 import { FaGithub } from "react-icons/fa";
 
-interface Props {
-  page: string;
-}
+export default function Header() {
+  const [query, setQuery] = useState("");
+  const router = useRouter();
 
-export default function Header({ page }: Props) {
+  const handleSearch = () => {
+    if (!query.trim()) return;
+    router.push(`/search?query=${encodeURIComponent(query)}`);
+  };
+
   return (
     <div className="flex flex-col bg-green-600 py-5">
       <div className="flex flex-row justify-between px-96 font-bold text-xl text-white">
@@ -16,12 +23,10 @@ export default function Header({ page }: Props) {
         <nav>
           <ul className="flex flex-row gap-2">
             <li className="hover:underline">
-              <Link href={page == "dataAnalysis" ? `/${page}` : "#"}>
-                Data Analysis
-              </Link>
+              <Link href="/dataAnalysis">Data Analysis</Link>
             </li>
             <li className="hover:underline">
-              <Link href={page == "about" ? `/${page}` : "#"}>About</Link>
+              <Link href="/about">About</Link>
             </li>
             <li className="hover:underline">
               <Link
@@ -40,9 +45,13 @@ export default function Header({ page }: Props) {
           <input
             className="w-4/12 h-14 pl-2 rounded-s-xl text-xl outline-none"
             type="text"
-            placeholder="Search..."
+            onChange={(e) => setQuery(e.target.value)}
+            placeholder="Search article by SNP"
           />
-          <button className="bg-green-900 h-14 w-14 rounded-e-xl flex items-center justify-center text-white hover:bg-green-800">
+          <button
+            onClick={handleSearch}
+            className="bg-green-900 h-14 w-14 rounded-e-xl flex items-center justify-center text-white hover:bg-green-800"
+          >
             <Search />
           </button>
         </div>
